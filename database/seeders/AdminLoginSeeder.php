bd68ab3337922d7189e320f94ff667118e820b14
savajidkt <savaji.dkt@gmail.com>
2022-10-06 01:05:01 +0530

    Admin Login Seeder

---
+++ database/seeders/AdminLoginSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminLoginSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Admin::create([
+            'name'=>'Savji Rathod',
+            'email'=>'admin@admin.com',
+            'password'=>Hash::make('123456'),
+            'type'=>1,
+            'status'=>1,
+            'created_at'=>Carbon::now(),
+            'updated_at'=>Carbon::now(),
+        ]);
+    }
+}
