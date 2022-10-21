4b064d64c20469c0e8744dad7aa3e303ab8a5b13
Savaji Rathod <savaji.dkt@gmail.com>
2022-10-21 14:26:16 +0530

    Title Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Bandelli Survey') }} | @yield('page_title')</title>
+    <title>@yield('page_title') | {{ config('app.name', 'Bandelli Survey') }} </title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Fonts -->
