c93418a52132cb5f7ba94a574cdbd28ee77f925d
savajidkt <savaji.dkt@gmail.com>
2022-11-11 22:16:23 +0530

    Formula code changes

--- resources/views/survey/question.blade.php
+++ resources/views/survey/question.blade.php
@@ -1,25 +1,41 @@
 <div class="multisteps_form_panel step sub-card">
     <h5 class="card-header animate__animated animate__fadeInRight animate_25ms">{{$model->question}}</h5>
-    <div class="text-primary mt-3">Change the order from which you are most likely to do and least likely by dragging the answers.</div>
+	<div class="text-primary mt-3">Change the order from which you are most likely to do and least likely by dragging the answers.</div>
     <div class="card-body">
         <input type="hidden" name="question_id" id="question_id" value="{{$model->id}}">
-
+       
+	   <div class="row">
+	   
+	   <div class="col-lg-2">
+	   <div class="customRadio-l-tag op-100 cr-t-m1">Most Likely</div>
+	   <div class="customRadio-l-tag op-80 cr-t-m2">Likely</div>
+	   <div class="customRadio-l-tag op-60 cr-t-m3">Neutral</div>
+	   <div class="customRadio-l-tag op-40 cr-t-m4">Unlikely </div>
+	   <div class="customRadio-l-tag op-25 cr-t-m5">Least likely</div>
+	   </div>
+	   
+	   <div class="col-lg-10">
         @if ($model->options->count())
         <div id="sorting-list">
-
+			
             @foreach ($model->options as $key=>$option)
-            <div class="">
-                <div class="customRadio-l-tag op-100">Most Likely</div>
-                <div class="customRadio-sec">
-                    <div class="customRadio">
-                        <input type="hidden" class="options" name="options[]" value="{{$option->id}}">
-                        <label for="radio1"> {{$option->option}}</label>
-                    </div>
-                </div>
+			<div class="customRadio-sec">
+            
+			<div class="customRadio">
+                <input type="hidden" class="options" name="options[]" value="{{$option->id}}">
+                <label for="radio1"> {{$option->option}}</label>
             </div>
+			
+			</div>
             @endforeach
-
+			
         </div>
         @endif
+		
+		</div>
+		
+		</div>
+		
+		
     </div>
 </div>
