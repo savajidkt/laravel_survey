9c3191d03f8e041b3a1246ba78d2fca89245d43d
Savaji Rathod <savaji.dkt@gmail.com>
2022-11-25 15:04:37 +0530

    High chart

--- resources/views/admin/pdf-reports/chart.blade.php
+++ resources/views/admin/pdf-reports/chart.blade.php
@@ -1,51 +1,134 @@
+<!DOCTYPE HTML>
 <html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Highcharts Example</title>
 
-<head>
-  <meta charset="utf-8" />
-  <title>Chart.js demo</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
-</head>
-<body>
+		<style type="text/css">
+.highcharts-figure, .highcharts-data-table table {
+    min-width: 310px; 
+    max-width: 800px;
+    margin: 1em auto;
+}
 
-  <h1>Chart.js Sample</h1>
+#container {
+    height: 400px;
+}
 
-  <canvas id="myChart" width="600" height="400"></canvas>
-  <script>
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: "polarArea",
-    
-      // The data for our dataset
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-          {
-            label: "My First dataset",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: [0, 10, 5, 2, 20, 30, 45]
-          }
-        ]
-      },
-    
-      // Configuration options go here
-      options: {
-        responsive:false,
-        maintainAspectRatio: false,
-        legend: {
-            display: true,
-            labels: {
-                fontColor: 'rgb(255, 99, 132)',
-                // generateLabels: function(chart)
-                // {
-                //     console.log(chart);
-                //     return `<p>Milan Soni</p>`;
-                // }
+.highcharts-data-table table {
+	font-family: Verdana, sans-serif;
+	border-collapse: collapse;
+	border: 1px solid #EBEBEB;
+	margin: 10px auto;
+	text-align: center;
+	width: 100%;
+	max-width: 500px;
+}
+.highcharts-data-table caption {
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
+}
+.highcharts-data-table th {
+	font-weight: 600;
+    padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+    padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+    background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+    background: #f1f7ff;
+}
+
+		</style>
+	</head>
+	<body>
+<script src="{{asset('highcharts.js')}}"></script>
+<script src="{{asset('modules/exporting.js')}}"></script>
+<script src="{{asset('modules/export-data.js"></script>
+<script src="{{asset('modules/accessibility.js"></script>
+
+<figure class="highcharts-figure">
+    <div id="container"></div>
+    <p class="highcharts-description">
+        Bar chart showing horizontal columns. This chart type is often
+        beneficial for smaller screens, as the user can scroll through the data
+        vertically, and axis labels are easy to read.
+    </p>
+</figure>
+
+
+
+		<script type="text/javascript">
+Highcharts.chart('container', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Historic World Population by Region'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+    },
+    xAxis: {
+        categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population (millions)',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
             }
         }
-      }
-    });
-  </script>
-</body>
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Year 1800',
+        data: [107, 31, 635, 203, 2]
+    }, {
+        name: 'Year 1900',
+        data: [133, 156, 947, 408, 6]
+    }, {
+        name: 'Year 2000',
+        data: [814, 841, 3714, 727, 31]
+    }, {
+        name: 'Year 2016',
+        data: [1216, 1001, 4436, 738, 40]
+    }]
+});
+		</script>
+	</body>
 </html>
